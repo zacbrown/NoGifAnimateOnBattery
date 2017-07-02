6128629f58826c71a99010eb58a3647915130370
Zac Brown <zac@zacbrown.io>
2017-07-02 14:07:38 -0600

    Initial Commit

---
+++ noanimateonbattery/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  noanimateonbattery
+//
+//  Created by Zac Brown on 7/2/17.
+//  Copyright © 2017 Zac Brown. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
